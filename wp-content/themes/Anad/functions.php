0b7807e7a0bc868cc1aac4a03a2cd938b4bf8b3c
Dana Sugu <dana.sugu@gmail.com>
2021-08-16 18:33:58 +0300

    cont.

--- wp-content/themes/Anad/functions.php
+++ wp-content/themes/Anad/functions.php
@@ -3,4 +3,5 @@ function anad_register_styles(){
   wp_enqueue_style('anad-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
 }
 
+add_action('wp_enqueue_scripts', 'anad_register_styles');
 ?>
