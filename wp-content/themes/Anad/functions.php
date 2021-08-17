e18968917d4238659cc4e86a50980ddfb735d230
Dana Sugu <dana.sugu@gmail.com>
2021-08-17 08:51:41 +0300

    Update functions.php

--- wp-content/themes/Anad/functions.php
+++ wp-content/themes/Anad/functions.php
@@ -2,13 +2,15 @@
 
 function anad_register_styles(){
 
+  $version = wp_get_theme()->get('Version');
+
   wp_enqueue_style('anad-bootstrap', 
-  "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all');
+  "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
 
   wp_enqueue_style('anad-fontawesome',  
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all');
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 
-  wp_enqueue_style('anad-style', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+  wp_enqueue_style('anad-style', get_template_directory_uri() . "/style.css", array(), $version, 'all');
 }
 
   add_action('wp_enqueue_scripts', 'anad_register_styles');
